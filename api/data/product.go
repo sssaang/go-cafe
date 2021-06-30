64bd113ebda815f7f6887ceadc8fc33324fc9f14
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-07-01 01:39:39 +0900

    feat: add product replace api

--- api/data/product.go
+++ api/data/product.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -65,4 +66,26 @@ func AddProduct(p *Product) {
 func getNextID() int {
 	lp := productList[len(productList) - 1]
 	return lp.ID + 1
+}
+
+func UpdateProduct(id int, prod *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	prod.ID = id
+	productList[pos] = prod
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found!")
+
+func findProduct(id int) (*Product, int, error)  {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
 }
--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/sssaang/go-cafe/api/data"
 )
@@ -23,6 +25,34 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		regex := regexp.MustCompile(`/([0-9]+)`)
+		g := regex.FindAllStringSubmatch(r.URL.Path, -1)
+		
+		if len(g) != 1 {
+			p.l.Println("Invalid URI: More than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI: More than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI: Unable to convert id to int")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
 		return
 	}
 
@@ -48,4 +78,19 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal data", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found!", http.StatusInternalServerError)
+	}
 }
