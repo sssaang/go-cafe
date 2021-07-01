e35f3eabcae38a9d290da8bb7091b08d65f4158d
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-07-02 02:22:26 +0900

    feat: add json validator and custom validator func

--- api/data/product.go
+++ api/data/product.go
@@ -4,18 +4,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"time"
+
+	"github.com/go-playground/validator"
 )
 
 type Product struct {
 	ID int `json:"id"`
-	Name string `json:"name"`
+	Name string `json:"name" validate:"required"`
 	Description string `json:"description"`
-	Price float32 `json:"price"`
-	SKU string `json:"sku"`
+	Price float32 `json:"price" validate:"gt=0"`
+	SKU string `json:"sku" validate:"required,sku"`
 	CreatedOn string `json:"-"`
 	UpdatedOn string `json:"-"`
 	DeletedOn string `json:"-"`
+}
+
+
+func (p *Product) Validate() error {
+	validate := validator.New()
+	validate.RegisterValidation("sku", valdiateSKU)
+	return validate.Struct(p)
+}
+
+func valdiateSKU(fl validator.FieldLevel) bool {
+	regex := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := regex.FindAllString(fl.Field().String(), -1)
+
+	return len(matches) != 1
 }
 
 func (p *Product) FromJSON(r io.Reader) error {
