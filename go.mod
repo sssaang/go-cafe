1a7ba203fbceffe60b4ff632638696f86faba463
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-07-07 22:13:52 +0900

    feat: add cors settings for potential client

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/sssaang/go-cafe
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-openapi/runtime v0.19.29 // indirect
+	github.com/go-openapi/runtime v0.19.29
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/sssaang/go-cafe/api/handlers"
 )
@@ -38,9 +39,12 @@ func main() {
 	deleteRouter := sm.Methods(http.MethodDelete).Subrouter()
 	deleteRouter.HandleFunc("/products/{id:[0-9]+}", ph.DeleteProduct)
 
+
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
+
 	s := &http.Server{
 		Addr: ":9090",
-		Handler: sm,
+		Handler: ch(sm),
 		IdleTimeout: 120 * time.Second,
 		ReadTimeout: 1 * time.Second,
 		WriteTimeout: 1 * time.Second,
