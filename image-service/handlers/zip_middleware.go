18c09c6df077c191304b6153daa2dce6b99a8e71
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-07-09 00:17:55 +0900

    feat: add gzip writer methods

--- image-service/handlers/zip_middleware.go
+++ image-service/handlers/zip_middleware.go
@@ -13,7 +13,10 @@ type GzipHandler struct {
 func (g *GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func (rw http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			
+			wr := NewWrappedResponseWriter(rw)
+			next.ServeHTTP(wr, r)
+			defer wr.Flush()
+
 			return
 		}
 
@@ -24,4 +27,27 @@ func (g *GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
 type WrappedResponseWriter struct {
 	rw http.ResponseWriter
 	gw *gzip.Writer
+}
+
+func NewWrappedResponseWriter(rw http.ResponseWriter) *WrappedResponseWriter {
+	gw := gzip.NewWriter(rw)
+
+	return &WrappedResponseWriter{rw: rw, gw: gw}
+}
+
+func (wr *WrappedResponseWriter) Header() http.Header {
+	return wr.rw.Header()
+}
+
+func (wr *WrappedResponseWriter) Write(d []byte) (int, error) {
+	return wr.gw.Write(d)
+}
+
+func (wr *WrappedResponseWriter) WriteHeader(statuscode int) {
+	wr.rw.WriteHeader(statuscode)
+}
+
+func (wr *WrappedResponseWriter) Flush() {
+	wr.gw.Flush()
+	wr.gw.Close()
 }
