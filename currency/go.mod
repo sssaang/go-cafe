e4e7caf13b621aec2f2cb9991099b8436643debb
Sangmin  Kim <you@example.com>
2021-07-15 03:09:29 +0900

    feat: implement currency handler

--- currency/go.mod
+++ currency/go.mod
@@ -3,7 +3,10 @@ module github.com/sssaang/go-cafe/currency
 go 1.16
 
 require (
-	google.golang.org/grpc v1.39.0 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/hashicorp/go-hclog v0.16.2
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/grpc v1.39.0
 	google.golang.org/protobuf v1.27.1 // indirect
 )
--- currency/main.go
+++ currency/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"net"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	protos "github.com/sssaang/go-cafe/currency/protos/currency"
+	"github.com/sssaang/go-cafe/currency/server"
+	"google.golang.org/grpc"
+)
+
 func main() {
+	log := hclog.Default()
+
+	gs := grpc.NewServer()
+	cs := server.NewCurrency(log)
+
+	protos.RegisterCurrencyServer(gs, cs)
 	
+	l, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		log.Error("Unable to listen", "error", err)
+		os.Exit(1)
+	}
+
+	gs.Serve(l)
 }
